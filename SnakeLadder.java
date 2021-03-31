96333a7ccd4c4811156bfd42bca2b8aa5d11ee81
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-03-31 22:32:38 +0530

    ADDED:added playing options

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,12 +1,31 @@
 import java.util.Random;
 
 public class SnakeLadder{
-    public static void main(String args[]){
-        Random random=new Random();
-        int diceRoll=random.nextInt(6);
-        int finalDiceRoll=diceRoll+1;
-        int position=0;
-        System.out.println("dice roll no is: "+finalDiceRoll);
+    public static void main(String args[]) {
+        Random random = new Random();
+
+        int diceRoll = random.nextInt(6);
+        int options = random.nextInt(3);
+        //Constants
+        int NO_PLAY = 0;
+        int LADDER = 1;
+        int SNAKE = 2;
+
+        //Variables
+        int finalDiceRoll = diceRoll + 1;
+        int position = 0;
+
+        System.out.println("dice roll no is: " + finalDiceRoll);
+        System.out.println("playing option is :"+options);
+        if (options == NO_PLAY)
+            position = position + 0;
+        else if (options == LADDER)
+            position = position + finalDiceRoll;
+        else {
+            position = position - finalDiceRoll;
+            if (position <= 0)
+                position = 0;
+        }
         System.out.println("current position:"+position);
     }
-}
+    }
