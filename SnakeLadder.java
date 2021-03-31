0ec3ec36535ca4e9c4316ae967d5398017ddd4ad
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-03-31 21:35:18 +0530

    Initializing start position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,6 @@
-public class SnakeLadder {
-	public static void main (String args[])
-	{
-		System.out.println("Welcome to the snake and ladder game");
-	}
+public class SnakeLadder{
+    public static void main(String args[]){
+        int position=0;
+        System.out.println("current position:"+position);
+    }
 }
