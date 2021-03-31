a0c05a6e946d1a3e20d5aa2089a4b20fb376038c
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-03-31 22:11:38 +0530

    The dice is rolled to get a number

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,12 @@
+import java.util.Random;
+
 public class SnakeLadder{
     public static void main(String args[]){
+        Random random=new Random();
+        int diceRoll=random.nextInt(6);
+        int finalDiceRoll=diceRoll+1;
         int position=0;
+        System.out.println("dice roll no is: "+finalDiceRoll);
         System.out.println("current position:"+position);
     }
 }
