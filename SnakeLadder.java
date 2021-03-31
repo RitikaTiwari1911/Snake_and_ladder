afff88ee9cb394374f461a2188eec9d5fd7bce20
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-01 00:23:32 +0530

    ADDED:the code shows the number of time dice was thrown and the position till it reached 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,31 +1,44 @@
 import java.util.Random;
 
-public class SnakeLadder{
+public class SnakeLadder {
     public static void main(String args[]) {
         Random random = new Random();
 
-        int diceRoll = random.nextInt(6);
-        int options = random.nextInt(3);
+
         //Constants
         int NO_PLAY = 0;
         int LADDER = 1;
         int SNAKE = 2;
+        int counter = 0;
 
         //Variables
-        int finalDiceRoll = diceRoll + 1;
         int position = 0;
 
-        System.out.println("dice roll no is: " + finalDiceRoll);
-        System.out.println("playing option is :"+options);
-        if (options == NO_PLAY)
-            position = position + 0;
-        else if (options == LADDER)
-            position = position + finalDiceRoll;
-        else {
-            position = position - finalDiceRoll;
-            if (position <= 0)
-                position = 0;
+        while (position < 100) {
+            counter++;
+
+            int diceRoll = random.nextInt(6);
+            int options = random.nextInt(3);
+            int finalDiceRoll = diceRoll + 1;
+            System.out.println("dice roll no is: " + finalDiceRoll);
+            System.out.println("playing option is :" + options);
+            if (options == NO_PLAY)
+                position = position + 0;
+            else if (options == LADDER)
+                if (position + finalDiceRoll > 100)
+                    position = position + 0;
+                else
+                    position = position + finalDiceRoll;
+
+            else {
+                position = position - finalDiceRoll;
+                if (position <= 0)
+                    position = 0;
+
+
+
+            }
+            System.out.println("dice roll no:" + counter + " current position:" + position);
         }
-        System.out.println("current position:"+position);
     }
-    }
+}
